6493c970f9c27c5c0c9c3f4976bd4677b94c7305
Arif Nur <nnarif37@gmail.com>
2023-12-08 06:26:05 +0700

    new search bar

--- pages/contents/main.tsx
+++ pages/contents/main.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import TableData from "./table/main";
-import SearchBar from "./search";
 import { Box } from "@chakra-ui/react";
 
 // import React from "react";
@@ -10,10 +9,7 @@ export default function Contents(props: any) {
   return (
     <>
       <Box p={ '5' }>
-        <Box m={ '5' } marginBottom={ '10' }>
-          <SearchBar data={ props.data } setData={ props.setData }/>
-        </Box>
-        <TableData data={ props.data }/>
+        <TableData data={ props.data } setData={ props.setData }/>
       </Box>
     </>
   )
--- pages/contents/table/main.tsx
+++ pages/contents/table/main.tsx
@@ -1,42 +1,52 @@
 "use client";
 
-import { TableContainer, Thead, Tr, Th, Td, Tbody, Table, Input } from "@chakra-ui/react"
-import { useState } from "react";
+import { TableContainer, Thead, Tr, Th, Td, Tbody, Table, Input, Box } from "@chakra-ui/react"
+import { useCallback, useState } from "react";
 
 const TableList = (props: any) => {
   const filteredData = props.data.filter(props.filterFunction);
+  return (
+    <Tbody>
+      {filteredData.map(({ nama_barang = '', satuan = '', harga = '' }, index: number) => {
+        return (
+          <>
+            <Tr key={index}>
+              <Td>{nama_barang}</Td>
+              <Td>{satuan}</Td>
+              <Td isNumeric>{harga}</Td>
+            </Tr>
+          </>
+        )
+      })}
+    </Tbody>
+  );
 }
 
 export default function TableData(props: any) {
   const [searchValue, setSearchValue] = useState("");
 
-	return (
-		<>
-			<Input placeholder='Cari Barang' type='text' value={ props.searchValue } onChange={ (event) => setSearchValue(event.target.value) }/>
-      <TableContainer overflowY={ 'auto' } maxHeight={ '300px' } minHeight={ 'calc(60vh)' }>
-        <Table variant={ 'simple' }>
-          <Thead position={ 'sticky' } top={ '0px' } bgColor={ '#161A30' }>
-						<Tr>
-							<Th textColor={ 'white' }>nama barang</Th>
-							<Th textColor={ 'white' }>satuan</Th>
-							<Th isNumeric textColor={ 'white' }>harga</Th>
-						</Tr>
-					</Thead>
-          <Tbody>
-            { props.data.map(({nama_barang='', satuan='', harga=''}, index: number) => {
-              return (
-                <>
-                  <Tr key={ index }>
-                    <Td>{ nama_barang }</Td>
-                    <Td>{ satuan }</Td>
-                    <Td isNumeric>{ harga }</Td>
-                  </Tr>
-                </>
-              )
-            }) }
-          </Tbody>
+  const filterFunction = useCallback(
+    (item) => item.nama_barang.toLowerCase().includes(searchValue.toLowerCase()),
+    [searchValue]
+  );
+
+  return (
+    <>
+      <Box p={ '5' }>
+        <Input placeholder='Cari Barang' type='text' value={searchValue} onChange={(event) => setSearchValue(event.target.value)} />
+      </Box>
+      <TableContainer overflowY={'auto'} maxHeight={'300px'} minHeight={'calc(60vh)'}>
+        <Table variant={'simple'}>
+          <Thead position={'sticky'} top={'0px'} bgColor={'#161A30'}>
+            <Tr>
+              <Th textColor={'white'}>nama barang</Th>
+              <Th textColor={'white'}>satuan</Th>
+              <Th isNumeric textColor={'white'}>harga</Th>
+            </Tr>
+          </Thead>
+          <TableList data={  props.data } filterFunction={ filterFunction }/>
         </Table>
       </TableContainer>
-		</>
-	)
+    </>
+  )
 }
